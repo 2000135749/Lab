504ae48a0970e790460094916dfc9373ccc091d7
2000135749 <Porselvi@hexaware.com>
2025-05-02 01:49:02 -0700

    Added class

---
+++ src/main/java/org/example/EmployeeFactory.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class EmployeeFactory {
+    public static void main(String[] args) {
+        Employee employee1 = new Employee(1,"Porselvi");
+        Employee employee2 = new Employee(1,"Ram");
+        Employee employee3 = new Employee(1,"Sita");
+
+    }
+}
